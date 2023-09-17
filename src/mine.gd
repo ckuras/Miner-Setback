b4531c0bbcc2ef1d866eb5bb61584496e7613bce
ckuras <kuras.chris@gmail.com>
2023-09-17 14:06:25 -0400

    Added cart movement and chunk generation

---
+++ src/mine.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var mine_chunks = $mine_chunks
+@onready var track: Path2D = $track
+
+@onready var mine_chunk = preload("res://Scenes/mine_chunk.tscn")
+
+const MINE_SIZE: int = 4
+
+func _ready():
+	for i in MINE_SIZE:
+		var mine_instance = mine_chunk.instantiate()
+		mine_instance.position.y = i * -160
+		mine_chunks.add_child(mine_instance)
+		track.curve.add_point(Vector2(0, i * -160))
+		
+
+
--- src/track.gd
+++ src/track.gd
@@ -2,7 +2,27 @@ extends Path2D
 
 @onready var main_cart = $PathFollow2D
 
-const CART_SPEED = 0.2
+var cart_speed: float = 1.0
+
+var cart_moving: bool = false
 
 func _process(_delta):
-	main_cart.progress += CART_SPEED
+	if main_cart.progress_ratio == 1.0:
+		cart_moving = false
+	if cart_moving:
+		main_cart.progress += cart_speed
+
+func _unhandled_input(event):
+	if event.is_action_pressed("stop") and cart_moving:
+		cart_moving = false
+	elif event.is_action_pressed("forward") and cart_moving:
+		cart_moving = false
+	elif event.is_action_pressed("reverse") and cart_moving:
+		cart_moving = false
+	elif event.is_action_pressed("forward") or event.is_action_pressed("stop") and !cart_moving:
+		cart_moving = true
+		cart_speed = 1.0
+	elif event.is_action_pressed("reverse") and !cart_moving:
+		cart_moving = true
+		cart_speed = abs(cart_speed) * -1
+
