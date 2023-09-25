260ecee1647d9239d1f7ebf589b1b3b4660c1390
ckuras <kuras.chris@gmail.com>
2023-09-25 19:16:20 -0400

    Added external inventory

--- src/inventory/inventory_interface.gd
+++ src/inventory/inventory_interface.gd
@@ -1,8 +1,10 @@
 extends Control
 
 var grabbed_slot_data: SlotData
+var external_inventory_owner
 
 @onready var cart_inventory: PanelContainer = $CartInventory
+@onready var external_inventory = $ExternalInventory
 @onready var grabbed_slot: PanelContainer = $GrabbedSlot
 
 func _physics_process(delta):
@@ -12,6 +14,15 @@ func _physics_process(delta):
 func set_cart_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_interact.connect(on_inventory_interact)
 	cart_inventory.set_inventory_data(inventory_data)
+
+func set_external_inventory(_external_inventory_owner) -> void:
+	external_inventory_owner = _external_inventory_owner
+	var inventory_data: InventoryData = external_inventory_owner.inventory_data
+	
+	inventory_data.inventory_interact.connect(on_inventory_interact)
+	external_inventory.set_inventory_data(inventory_data)
+	
+	external_inventory.show()
 
 func on_inventory_interact(inventory_data: InventoryData,
 		index: int, button: int) -> void:
@@ -38,6 +49,3 @@ func update_grabbed_slot() -> void:
 		grabbed_slot.set_slot_data(grabbed_slot_data)
 	else:
 		grabbed_slot.hide()
-
-func set_external_inventory(external_inventory_owner) -> void:
-	print(external_inventory_owner)
