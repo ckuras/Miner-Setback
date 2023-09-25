f254726a005abc1d4b71f9f958acbfa46f165488
ckuras <kuras.chris@gmail.com>
2023-09-24 20:56:57 -0400

    Added moving items in inventory

--- src/inventory/inventory_interface.gd
+++ src/inventory/inventory_interface.gd
@@ -2,7 +2,12 @@ extends Control
 
 var grabbed_slot_data: SlotData
 
-@onready var cart_inventory = $CartInventory
+@onready var cart_inventory: PanelContainer = $CartInventory
+@onready var grabbed_slot: PanelContainer = $GrabbedSlot
+
+func _physics_process(delta):
+	if grabbed_slot.visible:
+		grabbed_slot.global_position = get_global_mouse_position() + Vector2(5, 5)
 
 func set_cart_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_interact.connect(on_inventory_interact)
@@ -14,5 +19,18 @@ func on_inventory_interact(inventory_data: InventoryData,
 	match [grabbed_slot_data, button]:
 		[null, MOUSE_BUTTON_LEFT]:
 			grabbed_slot_data = inventory_data.grab_slot_data(index)
+		[_, MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.drop_slot_data(grabbed_slot_data, index)
+		[null, MOUSE_BUTTON_RIGHT]:
+			pass
+		[_, MOUSE_BUTTON_RIGHT]:
+			grabbed_slot_data = inventory_data.drop_single_slot_data(grabbed_slot_data, index)
 	
-	print(grabbed_slot_data)
+	update_grabbed_slot()
+
+func update_grabbed_slot() -> void:
+	if grabbed_slot_data:
+		grabbed_slot.show()
+		grabbed_slot.set_slot_data(grabbed_slot_data)
+	else:
+		grabbed_slot.hide()
